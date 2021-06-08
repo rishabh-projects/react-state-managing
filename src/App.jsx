eee7ccbba0d74de8655e89873965938056219a18
Rishabh Mewar <rmewar.dev@gmail.com>
2021-06-08 05:57:40 +0530

    cofigure "errorBoundary", "fetching & storing data via useEffect"

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import Footer from "./Footer";
 import Header from "./Header";
-
-const products = [
-  {
-    "id": 1,
-    "category": "shoes",
-    "image": "shoe1.jpg",
-    "name": "Hiker",
-    "price": 94.95,
-    "skus": [
-      { "sku": "17", "size": 7 },
-      { "sku": "18", "size": 8 }
-    ],
-    "description": "This rugged boot will get you up the mountain safely."
-  },
-  {
-    "id": 2,
-    "category": "shoes",
-    "image": "shoe2.jpg",
-    "name": "Climber",
-    "price": 78.99,
-    "skus": [
-      { "sku": "28", "size": 8 },
-      { "sku": "29", "size": 9 }
-    ],
-    "description": "Sure-footed traction in slippery conditions."
-  },
-  {
-    "id": 3,
-    "category": "shoes",
-    "image": "shoe3.jpg",
-    "name": "Explorer",
-    "price": 145.95,
-    "skus": [
-      { "sku": "37", "size": 7 },
-      { "sku": "38", "size": 8 },
-      { "sku": "39", "size": 9 }
-    ],
-    "description": "Look stylish while stomping in the mud."
-  }
-]
+import { getProducts } from "./services/productService";
+import Spinner from "./Spinner";
 
 export default function App() {
+
+  const [size, setSize] = useState("");
+  const [products, setProducts] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    getProducts("shoes")
+     .then((response) => setProducts(response))
+     .catch((e) => setError(e))
+     .finally(() => setLoading(false));
+  }, []);
+
   function renderProduct(p) {
     return (
       <div key={p.id} className="product">
@@ -60,6 +35,9 @@ export default function App() {
     ? products.filter((p) => p.skus.find((s) => s.size === parseInt(size)))
     : products;
 
+  if (error) throw error;
+  if (loading) return <Spinner />;
+  
   return (
     <>
       <div className="content">
